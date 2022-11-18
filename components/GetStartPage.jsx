b2ab74c696110e0ff86f48dc9836ecb58b585038
huzefa zeenwala <huzefa.zeenwala53@gmail.com>
2022-11-18 06:56:10 +0530

    add new logo

---
+++ components/GetStartPage.jsx
@@ -0,0 +1,11 @@
+
+export default function GetStartPage() {
+  return (
+    <div>
+      <nav>
+      <img src="/logoHalf.png" alt="" className="object-contain w-28" />
+      </nav>
+    </div>
+  )
+}
+
