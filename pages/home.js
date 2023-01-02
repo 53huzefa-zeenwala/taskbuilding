388b02a5beecb1839da4210103c20df50ccf7db4
huzefa zeenwala <huzefa.zeenwala53@gmail.com>
2023-01-02 18:37:48 +0530

    minor changes

--- pages/home.js
+++ pages/home.js
@@ -6,13 +6,17 @@ import { AddTaskButtonAndModel, Blob, CategoryMenu } from "../components";
 
 export default function home() {
   const { currentUser, userProfileData, openCategoryMenu, setOpenCategoryMenu } = useStateContext();
-  const { replace } = useRouter();
+  const { replace, reload } = useRouter();
   if (currentUser === undefined) {
     replace("/login");
   } 
-  if (userProfileData === undefined) {
+  if (userProfileData != undefined && userProfileData?.isProfileDataAdded === false) {
     replace("/newprofile")
   }
+  if (userProfileData?.avatarUrl === undefined && userProfileData != undefined) {
+      reload()
+  }
+  console.log(userProfileData, currentUser)
   return (
     <div className="relative overflow-x-hidden h-screen">
       <CategoryMenu {...{openCategoryMenu, setOpenCategoryMenu, userProfileData}} />
